3c2044ca2ea122319b31540034130939bca048fa
Ian Ibbotson <ianibbo@googlemail.com>
2011-09-05 16:48:10 +0100

    Created tabbed layout for main area of screen

--- src/me/ianibbo/ManageStations.java
+++ src/me/ianibbo/ManageStations.java
@@ -2,14 +2,35 @@ package me.ianibbo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.TabActivity;
+import android.widget.TabHost;
 
-public class ManageStations extends Activity
+public class ManageStations extends TabActivity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        TabHost.TabSpec spec=getTabHost().newTabSpec("about");
+        spec.setContent(R.id.about);
+        spec.setIndicator("About");
+        getTabHost().addTab(spec);
+
+        spec=getTabHost().newTabSpec("stations");
+        spec.setContent(R.id.stations);
+        spec.setIndicator("Stations");
+        getTabHost().addTab(spec);
+
+        spec=getTabHost().newTabSpec("news");
+        spec.setContent(R.id.news);
+        spec.setIndicator("News");
+        getTabHost().addTab(spec);
+
+        getTabHost().setCurrentTab(0);
+
     }
 }
